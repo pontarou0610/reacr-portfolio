5f28ab1e547569808685ee1541842b50552b24b1
pontarou0610 <pontarou0610@gmail.com>
2023-10-31 18:24:59 +0900

    firstcommit

---
+++ src/components/BlogPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+        <h1>ブログ</h1>
+
+        <p>
+            Lorem
+            Lorem
+        
+
+
+        </p>
+    </div>
+  )
+}
+
+export default BlogPage
